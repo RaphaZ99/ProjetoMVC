383f22ce96d922717cee05baf944f394e589e155
Raphael Mota <raphael.mota14@hotmail.com>
2019-10-29 11:50:03 -0300

    Primeiro model-controller -Departament

---
+++ SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace SalesWebMvc.Models
+{
+    public class Departament
+    {
+
+        public int Id { get; set; }
+        public string  Name { get; set; }
+
+    }
+}
---
+++ SalesWebMvc/Models/DepartamentsController.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Models
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Name = "Eletronicos" });
+            list.Add(new Departament { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
