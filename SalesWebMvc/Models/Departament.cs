5f0d452133a0b3f46d6a6e3542beaa73c1d12bc7
Raphael Mota <raphael.mota14@hotmail.com>
2019-11-02 10:25:20 -0300

    Adiocionando outras Entidades

--- SalesWebMvc/Models/Departament.cs
+++ SalesWebMvc/Models/Departament.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace SalesWebMvc.Models
 {
@@ -7,6 +9,35 @@ namespace SalesWebMvc.Models
 
         public int Id { get; set; }
         public string  Name { get; set; }
+        public ICollection <Seller> Sellers { get; set; } = new List<Seller>();
 
+
+        public Departament() { }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+
+            Sellers.Add(seller);
+
+        }
+
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+
+            //Pega a lista de vendedores(Seller) chama o .Sum() dentro do Linq Chama o Objeto da classe Seller
+            //Chama o Metodo TotalSales pegando a soma do tempo entre o tempo inicial e final da classe Seller
+            return Sellers.Sum(Seller => Seller.TotalSales(initial, final));
+
+
+        }
+        
+     }
     }
-}
+
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+
+        public int Id { get; set; }
+        public String  Name { get; set; }
+        public String  Email { get; set; }
+        public DateTime BirthDate  { get; set; }
+        public double  BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Departament departament)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        //Adiciona Vendas
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+
+        //Remeve Vendas
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+
+        }
+
+            public double TotalSales(DateTime initial, DateTime final)
+        {
+             //Where + expressão lambda para filtrar as datas do  vendedor da lista
+             //.Sum para somar o valor total as vendas de todos os vendedores
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+
+        }
+
+
+    }
+}
